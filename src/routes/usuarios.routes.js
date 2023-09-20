70523cbb43b64438aa54203a983145fe40c5ec11
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-09-20 11:53:43 -0600

    Se crea archivo para el mejod de rutas

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 import {
   getUsuarios,
   createUsuario,
+  updateEstadoUsuario,
+  getUsuarioById,
   updateUsuario,
-  deleteUsuario,
 } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
 router.get("/usuarios", getUsuarios);
+router.get("/usuarios/:id_usuario", getUsuarioById);
 router.post("/usuarios", createUsuario);
-router.put("/usuarios", updateUsuario);
-router.delete("/usuarios", deleteUsuario);
+router.patch("/usuarios/:id_usuario", updateEstadoUsuario);
+router.put("/usuarios/:id_usuario", updateUsuario);
 
 export default router;
