b3f619fb64e932dbe4c69ac3b5c3c0352eefb109
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-09-21 18:58:48 -0600

    Se crea ruta de acceso para auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import usuariosRoutes from "./routes/usuarios.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 import { PORT } from "./config.js";
 
 const app = express();
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api", usuariosRoutes);
 
 app.use((req, res, next) => {
