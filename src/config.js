124679e548ece4a18d5b6dacad9ca3ffe9975927
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-09-21 18:53:41 -0600

    Se agrega secret jwt en el archivo de config

--- src/config.js
+++ src/config.js
@@ -8,3 +8,4 @@ export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "123";
 export const DB_DATABASE = process.env.DB_DATABASE || "consultorio";
+export const SECRET_JWT_SEED = process.env.SECRET_JWT_SEED;
