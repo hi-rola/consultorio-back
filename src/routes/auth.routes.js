35384d83b7d6ba5b64327f7d7c5ee4335f5d9d12
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-10-06 13:08:40 -0600

    Se agregan puntos de entrada a las rutas

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,7 +8,7 @@ import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
-router.post("/usuarios", createUsuario);
+router.post("/registrar", createUsuario);
 router.post("/login", login);
 router.get("/renovarToken", validarJWT, renovarToken);
 
--- src/routes/consultas.routes.js
+++ src/routes/consultas.routes.js
@@ -4,6 +4,9 @@ import {
   getConsultasById,
   updateEstadoConsulta,
   createConsulta,
+  getConsultasPorFecha,
+  updateConsulta,
+  getInformacionUsuarioConsulta,
 } from "../controllers/consultas.controller.js";
 
 import { validarJWT } from "../middlewares/validar-jwt.js";
@@ -12,7 +15,14 @@ const router = Router();
 
 router.get("/consultas", validarJWT, getConsultas);
 router.get("/consultas/:id_consulta", validarJWT, getConsultasById);
+router.get("/consultas-fecha/:fecha", validarJWT, getConsultasPorFecha);
+router.get(
+  "/consultas-info/:id_consulta",
+  validarJWT,
+  getInformacionUsuarioConsulta
+);
 router.patch("/consultas/:id_consulta", validarJWT, updateEstadoConsulta);
 router.post("/consultas", validarJWT, createConsulta);
+router.put("/consultas/:id_consulta", validarJWT, updateConsulta);
 
 export default router;
